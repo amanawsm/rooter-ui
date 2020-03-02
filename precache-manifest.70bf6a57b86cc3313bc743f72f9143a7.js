8efeae3636c3ee0352bff8259aa7642c293100f7
aman <sandhuharjodh2561@seasiainfotech.com>
2020-03-02 11:20:27 +0530

    added files

---
+++ precache-manifest.70bf6a57b86cc3313bc743f72f9143a7.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8c6c54614764508c1effa42fce3d900",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8210af83954d4e0bf864",
+    "url": "/static/css/4.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6e4a98701bf5226ac1c8",
+    "url": "/static/css/main.ed8bcc39.chunk.css"
+  },
+  {
+    "revision": "a887929641c8354a7551",
+    "url": "/static/js/0.acf8cdaf.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/0.acf8cdaf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e43837f867949bfbf710",
+    "url": "/static/js/1.e5a22a88.chunk.js"
+  },
+  {
+    "revision": "133fb8975065882b4fe7",
+    "url": "/static/js/10.5562eb3c.chunk.js"
+  },
+  {
+    "revision": "a977ebe8e2090d1c29bc",
+    "url": "/static/js/11.765ffc68.chunk.js"
+  },
+  {
+    "revision": "8210af83954d4e0bf864",
+    "url": "/static/js/4.ed6e6a26.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/4.ed6e6a26.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09ba8c236375074c17b4",
+    "url": "/static/js/5.6575adcf.chunk.js"
+  },
+  {
+    "revision": "d1b4030bdbeedc277727",
+    "url": "/static/js/6.81dc3173.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/6.81dc3173.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d6e062cbf2cfc5376f7",
+    "url": "/static/js/7.c0aedb25.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/7.c0aedb25.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d204b7c0859f9345cdd",
+    "url": "/static/js/8.cb310ac2.chunk.js"
+  },
+  {
+    "revision": "72d1bab0f025e0c380fb",
+    "url": "/static/js/9.0febf11f.chunk.js"
+  },
+  {
+    "revision": "6e4a98701bf5226ac1c8",
+    "url": "/static/js/main.58e3c240.chunk.js"
+  },
+  {
+    "revision": "8fbea435c0e81f2ca6f8",
+    "url": "/static/js/runtime-main.d9d4a715.js"
+  },
+  {
+    "revision": "336daae2d8e0a79b879ccdb8118f664d",
+    "url": "/static/media/logo.336daae2.png"
+  }
+]);
